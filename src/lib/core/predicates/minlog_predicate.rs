dbc52b6015f2c7629ea2859e969c1c67a3fa94df
wedlichn <43807121+NoahWedlich@users.noreply.github.com>
2025-10-19 16:50:01 +0200

    Fleshed out the MinlogPredicate enum

--- src/lib/core/predicates/minlog_predicate.rs
+++ src/lib/core/predicates/minlog_predicate.rs
@@ -3,20 +3,60 @@ use std::rc::Rc;
 use crate::utils::pretty_printer::{PrettyPrintable, PPElement};
 
 use crate::core::types::minlog_type::MinlogType;
+use crate::core::terms::minlog_term::MinlogTerm;
+use crate::core::formulas::minlog_formula::MinlogFormula;
+
+#[derive(PartialEq, Eq, Clone)]
+pub struct PredicateDegree {
+    positive_content: bool,
+    negative_content: bool,
+}
 
 crate::wrapper_enum! {
     
     @default { EmptyPredicateBody }
     pub trait PredicateBody: PrettyPrintable, Clone, PartialEq, Eq {
         pub fn arity(&Self) -> Vec<Rc<MinlogType>>
+        
+        pub fn degree(&Self) -> PredicateDegree {
+            PredicateDegree { positive_content: true, negative_content: true }
+        }
+        
+        pub fn get_free_variables(&Self) -> Vec<Rc<MinlogTerm>> {
+            vec![]
+        }
+        
+        pub fn get_bound_variables(&Self) -> Vec<Rc<MinlogTerm>> {
+            vec![]
+        }
+        
+        pub fn get_type_variables(&Self) -> Vec<Rc<MinlogType>> {
+            vec![]
+        }
+        
+        pub fn get_predicate_variables(&Self) -> Vec<Rc<MinlogPredicate>> {
+            vec![]
+        }
+        
+        pub fn get_comprehension_terms(&Self) -> Vec<Rc<MinlogPredicate>> {
+            vec![]
+        }
+        
+        pub fn get_inductive_predicates(&Self) -> Vec<Rc<MinlogPredicate>> {
+            vec![]
+        }
+        
+        pub fn get_formulas(&Self) -> Vec<Rc<MinlogFormula>> {
+            vec![]
+        }
     }
     
     #[derive(PartialEq, Eq)]
     pub enum MinlogPredicate {
-        Constant(|constant|),
-        Variable(|variable|),
-        Comprehension(|comprehension|),
-        Inductive(|inductive|),
+        Constant(||constant||),
+        Variable(||variable||),
+        Comprehension(||comprehension||),
+        Inductive(||inductive||),
     }
     
     impl PrettyPrintable {
@@ -36,7 +76,7 @@ impl MinlogPredicate {
         let mut type_vars = vec![];
         
         for t in types {
-            for var in MinlogType::get_type_variables(&t) {
+            for var in t.get_type_variables() {
                 if !type_vars.contains(&var) {
                     type_vars.push(var);
                 }
@@ -44,6 +84,10 @@ impl MinlogPredicate {
         }
         
         type_vars
+    }
+    
+    pub fn to_cterm(pred: &Rc<MinlogPredicate>) -> Rc<MinlogPredicate> {
+        todo!()
     }
 }
 
